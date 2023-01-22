4957638affe9f162f3f112f49e0121b40af95d07
mustafasevim <mustafasevim@protonmail.com>
2023-01-22 11:20:27 +0300

    Update temperature_control_raspberrypi.py

--- temperature_control_raspberrypi.py
+++ temperature_control_raspberrypi.py
@@ -11,8 +11,13 @@ URL = "http://esp8266.local/command"
 
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if temperature > 21.5:
-        requests.get(URL + "/disable")
-    elif temperature < 20.5:
-        requests.get(URL + "/enable")
+    if humidity is not None and temperature is not None:
+        humidity = round(humidity, 1)
+        temperature = round(temperature, 1)
+        if temperature > 21.5:
+            requests.get(URL + "/disable")
+        elif temperature < 20.5:
+            requests.get(URL + "/enable")
+    else:
+        print('Failed to get reading. Try again!')
     time.sleep(60)
